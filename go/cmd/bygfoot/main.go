4e1bd2e747a3be7fe7c07f05a17fac2cb335f639
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2021-04-03 22:25:19 +0100

    Added association table and db migration

--- go/cmd/bygfoot/main.go
+++ go/cmd/bygfoot/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2/app"
+	"github.com/kashifsoofi/bygfoot-go/db"
 	"github.com/kashifsoofi/bygfoot-go/ui"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	sqliteDb, err := db.NewDB("bygfoot.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqliteDb.Close()
+
+	err = db.RunMigrations(sqliteDb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	a := app.New()
 	w := ui.NewSplashWindow(a)
 
--- go/go.mod
+++ go/go.mod
@@ -1,9 +1,11 @@
 module github.com/kashifsoofi/bygfoot-go
 
-go 1.15
+go 1.16
 
 require (
 	fyne.io/fyne v1.4.3
 	fyne.io/fyne/v2 v2.0.1
+	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/google/uuid v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.6
 )
