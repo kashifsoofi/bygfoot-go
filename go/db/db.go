75e9eb3223b511d3f3f800b7fe8e2a9b3a069fd0
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2021-04-03 22:28:13 +0100

    Added comment

--- go/db/db.go
+++ go/db/db.go
@@ -30,6 +30,8 @@ func RunMigrations(db *sql.DB) error {
 		return fmt.Errorf("creating sqlite3 db driver failed %w", err)
 	}
 
+	// https://github.com/golang-migrate/migrate/issues/471
+	// temp solution would update to use embed source once merged
 	source, err := httpfs.New(http.FS(migrations), "migrations")
 	if err != nil {
 		return fmt.Errorf("error initialising source %w", err)
