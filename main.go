b6e42dbccd4a475e3df3869648a3fa694a0d2fe8
Kashif Soofi <kashif.soofi@constructionline.co.uk>
2022-01-20 14:48:52 +0000

    Add log level command line flag

--- main.go
+++ main.go
@@ -5,15 +5,37 @@ import (
 
 	"github.com/kashifsoofi/bygfoot-go/ui"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/jessevdk/go-flags"
 )
 
+type Options struct {
+	// 2 = Error, 5 = Debug
+	LogLevel log.Level `short:"l" long:"loglevel" description:"Set current log level" default:"5"`
+}
+
+var options Options
+var parser = flags.NewParser(&options, flags.Default)
+
 func init() {
+	if _, err := parser.Parse(); err != nil {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		default:
+			os.Exit(1)
+		}
+	}
+
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(options.LogLevel)
 }
 
 func main() {
-	log.Debug("main")
+	log.Debug("main debug")
 
 	app := ui.NewApp(nil)
 	app.Run()
